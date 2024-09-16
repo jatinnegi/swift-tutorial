b5821488af79cee3b291febd5ce82cbd159bb2ee
jatinnegi <negijatinn@gmail.com>
2024-09-17 01:11:21 +0530

    Strings, numbers, decimals, boolean, string interpolation

---
+++ boolean/MyPlayground.playground/Contents.swift
@@ -0,0 +1,19 @@
+let filename = "paris.jpg"
+// print(filename.hasSuffix(".jpg"))
+
+let number = 120
+// print(number.isMultiple(of: 3))
+
+let goodDogs = true
+var gameOver = false
+print(gameOver)
+gameOver.toggle()
+print(gameOver)
+
+let isMultiple = 120.isMultiple(of: 3)
+
+var isAuthenticated = false
+print(isAuthenticated)
+
+isAuthenticated = !isAuthenticated
+print(isAuthenticated)
---
+++ string-interpolation/MyPlayground.playground/Contents.swift
@@ -0,0 +1,21 @@
+let firstPart = "Hello, "
+let secondPart = "world!"
+let greeting = firstPart + secondPart
+
+print(greeting)
+
+let luggageCode = "1" + "2" + "3" + "4" + "5"
+
+let quote = "Then he tapped a sign saying \"Believe\" and walked away."
+print(quote)
+
+let name = "Taylor"
+let age = 26
+let message = "Hello, my name is \(name) and I'm \(age) years old."
+print(message)
+
+let number = 11
+let missionMessage = "Apollo \(number) landed on the moon."
+print(missionMessage)
+
+print("5 x 5 = \(5 * 5)")
