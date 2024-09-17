bb15dfcb9efe88db43b5bc8073b9d47cf52e9615
jatinnegi <negijatinn@gmail.com>
2024-09-17 22:39:50 +0530

    Checkpoint 1

---
+++ checkpoint-1/MyPlayground.playground/Contents.swift
@@ -0,0 +1,5 @@
+let tempCelsius = 25.5
+let tempFahrenheit = (tempCelsius * (9.0 / 5.0)) + 32
+
+print("Temperature in Celsius    = \(tempCelsius)°C")
+print("Temperature in Fahrenheit = \(tempFahrenheit)°F")
